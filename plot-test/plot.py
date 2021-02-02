e2e96d350c3f555928dff66271ce698fbcf9b85a
RicardoATB <ricardoatb@gmail.com>
2021-02-02 16:54:13 -0500

    2 more days to finish QNX

--- plot-test/plot.py
+++ plot-test/plot.py
@@ -3,10 +3,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Must set figsize before plotting it
+plt.figure(figsize=(30,30))
+
 data = np.loadtxt("qnx.txt")
 
 x, y = data.T
 
 plt.plot(*data.T)
 plt.gca().invert_yaxis()
+plt.axis('equal')
 plt.show()
