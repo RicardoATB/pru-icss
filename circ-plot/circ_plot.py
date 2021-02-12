b1a808231066381cfa9c90edb224a5544da65d0e
RicardoATB <ricardoatb@gmail.com>
2021-02-12 08:52:48 -0500

    plotting right! todo: horizontal bottom side

--- circ-plot/circ_plot.py
+++ circ-plot/circ_plot.py
@@ -4,12 +4,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 from math import sin, cos
 
+internal_angles = []
 num_vert = int(input("Enter number of vertices: "))
-radius = int(input("Enter maximum size (diameter): "))/2
+radius = float(input("Enter maximum size (diameter): "))/2
 vert_angle = 360/num_vert
 
 # creating iterable list of all internal angles
-internal_angles = [i for i in range(0, 360) if i % vert_angle == 0]
+#internal_angles = [i for i in range(0, 360) if i % vert_angle == 0]
+for i in range(0, num_vert+1):
+	internal_angles.insert(i, float(i*vert_angle))
 
 with open ("coordinates.txt", "w") as f_out:
 	for i in internal_angles:
@@ -32,26 +35,16 @@ with open ("coordinates.txt", "w") as f_out:
 		if (i > 270 and i <= 360):
 			x = radius * cos(np.deg2rad(i))
 			y = radius * sin(np.deg2rad(i))		
-		
-		# saving first coordinate to be added later as the last
-		if (internal_angles.index(i) == 0):
-			first_x = x
-			first_y = y	
 	
 		print("x = ", round(x,2),", y = ", round(y,2),"\n")
 		f_out.write(str(round(x,2)) + " " + str(round(y,2)) + "\n")
-		
-		# if it's the last coordinate, append the value of the first one
-		if (internal_angles.index(i) == (len(internal_angles) - 1)):
-			f_out.write(str(round(first_x,2)) + " " + str(round(first_y,2)) + "\n")
-
 
 # ploting
 # Must set figsize before plotting it
 plt.figure(figsize=(30,30))
 data = np.loadtxt("coordinates.txt")
 x, y = data.T
-plt.plot(*data.T, linewidth=3)
+plt.plot(*data.T, linewidth=3, marker=".", markersize=40, markerfacecolor='r')
 #plt.gca().invert_yaxis()
 plt.axis('equal')
 plt.show()
