3a5d13d2d5929a05cd5d4303f0501e45c5ac6da1
RicardoATB <ricardoatb@gmail.com>
2021-02-12 07:55:25 -0500

    writing to the file

--- circ-plot/circ_plot.py
+++ circ-plot/circ_plot.py
@@ -19,32 +19,31 @@ plt.show()
 
 num_vert = int(input("Enter number of vertices: "))
 radius = int(input("Enter maximum size (diameter): "))/2
-print("\n")
 vert_angle = 360/num_vert
 
 # creating iterable list of all internal angles
 int_angles = [i for i in range(0, 360) if i % vert_angle == 0]
 
-for i in int_angles:
-	print ("For angle ", i, ":")
-	# quadrant 1
-	if (i <= 90):
-		x = radius * cos(np.deg2rad(i))
-		y = radius * sin(np.deg2rad(i))
-	# quadrant 2
-	if (i <= 180 and i >90):
-		q2_i = 90 - i;
-		x = -radius * cos(np.deg2rad(q2_i))
-		y = -radius * sin(np.deg2rad(q2_i))
-	# quadrant 3
-	if (i > 180 and i <= 270):
-		q3_i = 180 - i
-		x = radius * sin(np.deg2rad(i))
-		y = radius * cos(np.deg2rad(i))
-	# quadrant 4
-	if (i > 270 and i <=360):
-		x = radius * cos(np.deg2rad(i))
-		y = radius * sin(np.deg2rad(i))
-				
-
-	print("x = ", round(x,2),", y = ", round(y,2),"\n")
+with open ("coordinates.txt", "w") as f_out:
+	for i in int_angles:
+		print ("\nFor angle ", i, ":")
+		# quadrant 1
+		if (i <= 90):
+			x = radius * cos(np.deg2rad(i))
+			y = radius * sin(np.deg2rad(i))
+		# quadrant 2
+		if (i <= 180 and i >90):
+			q2_i = 90 - i;
+			x = -radius * cos(np.deg2rad(q2_i))
+			y = -radius * sin(np.deg2rad(q2_i))
+		# quadrant 3
+		if (i > 180 and i <= 270):
+			q3_i = 180 - i
+			x = radius * sin(np.deg2rad(i))
+			y = radius * cos(np.deg2rad(i))
+		# quadrant 4
+		if (i > 270 and i <=360):
+			x = radius * cos(np.deg2rad(i))
+			y = radius * sin(np.deg2rad(i))		
+		print("x = ", round(x,2),", y = ", round(y,2),"\n")
+		f_out.write(str(round(x,2)) + " " + str(round(y,2)) + "\n")
