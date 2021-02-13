64fe537c40162154c6f7132d1f54ffc9a3e7d4e0
RicardoATB <ricardoatb@gmail.com>
2021-02-13 08:37:18 -0500

    missing comp center now

--- circ-plot/circ_plot.py
+++ circ-plot/circ_plot.py
@@ -4,25 +4,49 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import numpy as np
 import os
-from math import sin, cos
+from math import sin, cos, sqrt
 
-def plot_component(q, x, y, angle):
+def slope(x, y, q):
+	if (q == 1 or q == 3):
+		return (y/x)
+	if (q == 2 or q == 4):
+		return (y/x)
+
+def comp_center_x(comp_y, x, y, q, m):
+	if (q == 1 or q == 4):
+		return (x + comp_y/(sqrt(1 + m*m)))
+	if (q == 2 or q == 3):
+		return (x - comp_y/(sqrt(1 + m*m)))
+
+def plot_component(q, comp_x, comp_y, x, y, angle):
 	# ploting components
-	#x1, y1 = [-1, 12], [1, 10]
-	#x2, y2 = [-1, 10], [3, -1]
-	#plt.plot(x1, y1, x2, y2, marker = "o")
-	# radius * cos(np.deg2rad(i))
 	if (q == 1):
-		rect = plt.Rectangle((x,y), comp_x/10, comp_y/10, angle, fc='blue', ec="red")
+		m = slope(x, y, q)
+		x_offset = comp_center_x(comp_y, x, y, q, m)
+		y_offset = m*x_offset
+		plt.plot(x_offset, y_offset, 'o', color='black', markersize = 20, markerfacecolor = 'g');
+		rect = plt.Rectangle((x,y), comp_x, comp_y, angle, fc='blue', ec="red")
 		plt.gca().add_patch(rect)
 	if (q == 2):
-		rect = plt.Rectangle((x,y), comp_x/10, comp_y/10, angle, fc='blue', ec="red")
+		m = slope(x, y, q)
+		x_offset = comp_center_x(comp_y, x, y, q, m)
+		y_offset = m*x_offset
+		plt.plot(x_offset, y_offset, 'o', color='black', markersize = 20, markerfacecolor = 'g');
+		rect = plt.Rectangle((x,y), comp_x, comp_y, angle, fc='blue', ec="red")
 		plt.gca().add_patch(rect)
 	if (q == 3):
-		rect = plt.Rectangle((x,y), comp_x/10, comp_y/10, angle, fc='blue', ec="red")
+		m = slope(x, y, q)
+		x_offset = comp_center_x(comp_y, x, y, q, m)
+		y_offset = m*x_offset
+		plt.plot(x_offset, y_offset, 'o', color='black', markersize = 20, markerfacecolor = 'g');
+		rect = plt.Rectangle((x,y), comp_x, comp_y, angle, fc='blue', ec="red")
 		plt.gca().add_patch(rect)
 	if (q == 4):
-		rect = plt.Rectangle((x,y), comp_x/10, comp_y/10, angle, fc='blue', ec="red")
+		m = slope(x, y, q)
+		x_offset = comp_center_x(comp_y, x, y, q, m)
+		y_offset = m*x_offset
+		plt.plot(x_offset, y_offset, 'o', color='black', markersize = 20, markerfacecolor = 'g');
+		rect = plt.Rectangle((x,y), comp_x, comp_y, angle, fc='blue', ec="red")
 		plt.gca().add_patch(rect)
 
 def plot_graph():
@@ -47,6 +71,10 @@ def plot_graph():
 def main():
 	plt.figure(figsize=(30,30))
 	internal_angles = []
+	num_vert = int(input("Enter number of vertices: "))
+	radius = float(input("Enter maximum size (diameter): "))/2
+	comp_x = float(input("Enter component width (x): "))
+	comp_y = float(input("Enter component heigh (y): "))
 	vert_angle = 360/num_vert
 
 	# creating iterable list of all internal angles
@@ -60,36 +88,31 @@ def main():
 				x = radius * cos(np.deg2rad(i))
 				y = radius * sin(np.deg2rad(i))
 				angle = i - 90
-				plot_component(1, x, y, angle)
+				plot_component(1, comp_x, comp_y, x, y, angle)
 			# quadrant II
 			if (i <= 180 and i > 90):
 				q2_i = 180 - i;
 				x = -radius * cos(np.deg2rad(q2_i))
 				y = radius * sin(np.deg2rad(q2_i))
 				angle = 90 - q2_i
-				plot_component(2, x, y, angle)
+				plot_component(2, comp_x, comp_y, x, y, angle)
 			# quadrant III
 			if (i > 180 and i <= 270):
 				q3_i = i - 180
 				x = radius * cos(np.deg2rad(i))
 				y = radius * sin(np.deg2rad(i))
 				angle = -270 + q3_i
-				plot_component(3, x, y, angle)
+				plot_component(3, comp_x, comp_y, x, y, angle)
 			# quadrant IV
 			if (i > 270 and i <= 360):
 				x = radius * cos(np.deg2rad(i))
 				y = radius * sin(np.deg2rad(i))
 				angle = i - 90	
-				plot_component(4, x, y, angle)
+				plot_component(4, comp_x, comp_y, x, y, angle)
 	
 			f_out.write(str("{:>5.2f}".format(round(x,2))) + " \t" + str("{:>5.2f}".format(round(y,2))) \
 			+ "\t# vertex " + str(internal_angles.index(i)+1) + " @ " + str("{:>7.2f}".format(angle)) + str("\u00b0\n"))
 
 	plot_graph()
-
-num_vert = int(input("Enter number of vertices: "))
-radius = float(input("Enter maximum size (diameter): "))/2
-comp_x = float(input("Enter component width (x): "))
-comp_y = float(input("Enter component heigh (y): "))
 
 main()
