ec57b660e91eac0cc02d378868a2001142d03398
RicardoATB <ricardoatb@gmail.com>
2021-02-13 11:48:02 -0500

    found q1 comp center

--- circ-plot/circ_plot.py
+++ circ-plot/circ_plot.py
@@ -4,7 +4,9 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import numpy as np
 import os
-from math import sin, cos, sqrt
+#from math import sin, cos, sqrt, atan, pi
+import math
+
 
 def slope(x, y, q):
 	if (q == 1 or q == 3):
@@ -14,38 +16,88 @@ def slope(x, y, q):
 
 def comp_center_x(comp_y, x, y, q, m):
 	if (q == 1 or q == 4):
-		return (x + comp_y/(sqrt(1 + m*m)))
+		return (x + comp_y/(math.sqrt(1 + m*m)))
 	if (q == 2 or q == 3):
-		return (x - comp_y/(sqrt(1 + m*m)))
+		return (x - comp_y/(math.sqrt(1 + m*m)))
 
-def plot_component(q, comp_x, comp_y, x, y, angle):
+"""
+x1=0;y1=0;x2=6;y2=0; % initial data
+alp1=2*pi/3;alp2=pi/6; % initial data
+u=x2-x1;v=y2-y1;a3=sqrt(u^2+v^2);
+alp3=pi-alp1-alp2;
+a2=a3*sin(alp2)/sin(alp3);
+RHS1=x1*u+y1*v+a2*a3*cos(alp1);
+RHS2=y2*u-x2*v-a2*a3*sin(alp1);
+x3=(1/a3^2)*(u*RHS1-v*RHS2);
+y3=(1/a3^2)*(v*RHS1+u*RHS2);
+"""
+
+x3 = y3 = 0
+
+def int_angle_comp():
+	 return math.atan((comp_x/2)/(comp_y/2))
+
+def comp_center_coord(x, y, comp_vert_x, comp_vert_y):
+	global x3, y3
+	offset_pair = []
+	alp1 = alp2 = alpha
+	alp3 = math.pi - 2*alpha
+	x1 = x
+	y1 = y
+	x2 = comp_vert_x
+	y2 = comp_vert_y
+	#x1=0;y1=0;x2=6;y2=0		 			# initial data
+	#alp1=2*pi/3;alp2=pi/6				# initial data
+	u = x2-x1
+	v = y2-y1
+	a3 = math.sqrt(u*u + v*v)	
+	#alp3=pi-alp1-alp2
+	a2 = a3 * math.sin(alp2)/math.sin(alp3)
+	RHS1 = x1*u + y1*v + a2*a3*math.cos(alp1)
+	RHS2 = y2*u - x2*v - a2*a3*math.sin(alp1)
+	
+	#offset_pair.append((1/a3*a3)*(u*RHS1 - v*RHS2))
+	#offset_pair.append((1/a3*a3)*(v*RHS1 + u*RHS2))
+	
+	x3 = (1/(a3*a3))*(u*RHS1 - v*RHS2)
+	y3 = (1/(a3*a3))*(v*RHS1 + u*RHS2)
+
+	print ("x3 = ", x3, ", y3 = ", y3)
+	#return offset_pair
+	
+def plot_component(q, x, y, angle):
 	# ploting components
 	if (q == 1):
 		m = slope(x, y, q)
-		x_offset = comp_center_x(comp_y, x, y, q, m)
-		y_offset = m*x_offset
-		plt.plot(x_offset, y_offset, 'o', color='black', markersize = 20, markerfacecolor = 'g');
+		comp_vert_x = comp_center_x(comp_y, x, y, q, m)
+		comp_vert_y = m*comp_vert_x
+		plt.plot(comp_vert_x, comp_vert_y, 'o', color='black', markersize = 20, markerfacecolor = 'g');
+		
+		comp_center_coord(x, y, comp_vert_x, comp_vert_y)
+		plt.plot(x3, y3, 'o', color='black', markersize = 20, markerfacecolor = [0,1,1,1])
+		
+
 		rect = plt.Rectangle((x,y), comp_x, comp_y, angle, fc='blue', ec="red")
 		plt.gca().add_patch(rect)
 	if (q == 2):
 		m = slope(x, y, q)
-		x_offset = comp_center_x(comp_y, x, y, q, m)
-		y_offset = m*x_offset
-		plt.plot(x_offset, y_offset, 'o', color='black', markersize = 20, markerfacecolor = 'g');
+		comp_vert_x = comp_center_x(comp_y, x, y, q, m)
+		comp_vert_y = m*comp_vert_x
+		plt.plot(comp_vert_x, comp_vert_y, 'o', color='black', markersize = 20, markerfacecolor = 'g');
 		rect = plt.Rectangle((x,y), comp_x, comp_y, angle, fc='blue', ec="red")
 		plt.gca().add_patch(rect)
 	if (q == 3):
 		m = slope(x, y, q)
-		x_offset = comp_center_x(comp_y, x, y, q, m)
-		y_offset = m*x_offset
-		plt.plot(x_offset, y_offset, 'o', color='black', markersize = 20, markerfacecolor = 'g');
+		comp_vert_x = comp_center_x(comp_y, x, y, q, m)
+		comp_vert_y = m*comp_vert_x
+		plt.plot(comp_vert_x, comp_vert_y, 'o', color='black', markersize = 20, markerfacecolor = 'g');
 		rect = plt.Rectangle((x,y), comp_x, comp_y, angle, fc='blue', ec="red")
 		plt.gca().add_patch(rect)
 	if (q == 4):
 		m = slope(x, y, q)
-		x_offset = comp_center_x(comp_y, x, y, q, m)
-		y_offset = m*x_offset
-		plt.plot(x_offset, y_offset, 'o', color='black', markersize = 20, markerfacecolor = 'g');
+		comp_vert_x = comp_center_x(comp_y, x, y, q, m)
+		comp_vert_y = m*comp_vert_x
+		plt.plot(comp_vert_x, comp_vert_y, 'o', color='black', markersize = 20, markerfacecolor = 'g');
 		rect = plt.Rectangle((x,y), comp_x, comp_y, angle, fc='blue', ec="red")
 		plt.gca().add_patch(rect)
 
@@ -71,10 +123,6 @@ def plot_graph():
 def main():
 	plt.figure(figsize=(30,30))
 	internal_angles = []
-	num_vert = int(input("Enter number of vertices: "))
-	radius = float(input("Enter maximum size (diameter): "))/2
-	comp_x = float(input("Enter component width (x): "))
-	comp_y = float(input("Enter component heigh (y): "))
 	vert_angle = 360/num_vert
 
 	# creating iterable list of all internal angles
@@ -85,34 +133,40 @@ def main():
 		for i in internal_angles:
 			# quadrant I
 			if (i <= 90):
-				x = radius * cos(np.deg2rad(i))
-				y = radius * sin(np.deg2rad(i))
+				x = radius * math.cos(np.deg2rad(i))
+				y = radius * math.sin(np.deg2rad(i))
 				angle = i - 90
-				plot_component(1, comp_x, comp_y, x, y, angle)
+				plot_component(1, x, y, angle)
 			# quadrant II
 			if (i <= 180 and i > 90):
 				q2_i = 180 - i;
-				x = -radius * cos(np.deg2rad(q2_i))
-				y = radius * sin(np.deg2rad(q2_i))
+				x = -radius * math.cos(np.deg2rad(q2_i))
+				y = radius * math.sin(np.deg2rad(q2_i))
 				angle = 90 - q2_i
-				plot_component(2, comp_x, comp_y, x, y, angle)
+				plot_component(2, x, y, angle)
 			# quadrant III
 			if (i > 180 and i <= 270):
 				q3_i = i - 180
-				x = radius * cos(np.deg2rad(i))
-				y = radius * sin(np.deg2rad(i))
+				x = radius * math.cos(np.deg2rad(i))
+				y = radius * math.sin(np.deg2rad(i))
 				angle = -270 + q3_i
-				plot_component(3, comp_x, comp_y, x, y, angle)
+				plot_component(3, x, y, angle)
 			# quadrant IV
 			if (i > 270 and i <= 360):
-				x = radius * cos(np.deg2rad(i))
-				y = radius * sin(np.deg2rad(i))
+				x = radius * math.cos(np.deg2rad(i))
+				y = radius * math.sin(np.deg2rad(i))
 				angle = i - 90	
-				plot_component(4, comp_x, comp_y, x, y, angle)
+				plot_component(4, x, y, angle)
 	
 			f_out.write(str("{:>5.2f}".format(round(x,2))) + " \t" + str("{:>5.2f}".format(round(y,2))) \
 			+ "\t# vertex " + str(internal_angles.index(i)+1) + " @ " + str("{:>7.2f}".format(angle)) + str("\u00b0\n"))
 
 	plot_graph()
+
+num_vert = int(input("Enter number of vertices: "))
+radius = float(input("Enter maximum size (diameter): "))/2
+comp_x = float(input("Enter component width (x): "))
+comp_y = float(input("Enter component heigh (y): "))
+alpha = int_angle_comp()
 
 main()
